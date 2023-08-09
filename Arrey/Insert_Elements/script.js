c7fbf3bdbba11d0d6f921c96cfa2769bcf9f0d56
Rup medhi <rupmmedhi@gmail.com>
2023-08-09 21:49:51 +0530

    insert elements dynamically

--- Arrey/Insert_Elements/script.js
+++ Arrey/Insert_Elements/script.js
@@ -1,12 +1,34 @@
+//insert elements
+
+// let data = [2, 10, 35, 48, 59];
+// let newEl = document.getElementById("newEl")
+// let position = document.getElementById("position")
+
+// for(let i= data.length-1; i>=0; i--){
+//     if(i>=position){
+//         data[i+1]=data[i];
+//         if(i===position){
+//             data[i]=newEl
+//         }
+//     }
+// }
+
+function insertEl(){
+  
 let data = [2, 10, 35, 48, 59];
-let newEl = 70;
-let position = 2;
+let newEl = document.getElementById("newEl").value
+let position = document.getElementById("position").value
+
+console.log(data)
 
 for(let i= data.length-1; i>=0; i--){
     if(i>=position){
         data[i+1]=data[i];
-        if(i===position){
+        if(i==position){
             data[i]=newEl
         }
     }
+}
+console.log(data)
+
 }
